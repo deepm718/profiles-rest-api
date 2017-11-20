9888702d4bc49794c5cbde32a34916a85d820b76
deepm718 <kuldeep.maurya@stribog.com>
2017-11-21 00:09:39 +0530

    Addedd profile listing and creating features.

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -8,8 +8,8 @@ from . import views
 
 
 router = DefaultRouter()
-
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 
 urlpatterns = [
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
-
+from . import models
 
 # Create your views here.
 
@@ -112,3 +112,10 @@ class HelloViewSet(viewsets.ViewSet):
 
         return Response({'http_method': 'DELETE'})
 
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
